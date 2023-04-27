402d430df1920230ecfd08a0110d24ef2c33d442
Bea Boxler <bea@boxler.me>
2023-04-27 15:05:15 +0100

    Better benchmarking insight

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -5,19 +5,21 @@ extern crate b64_rs;
 use b64_rs::{decode, encode};
 use std::time::Instant;
 
-const COUNT: f64 = 1000.0;
+const COUNT: usize = 1000;
 
 fn main() {
     let text = load_text();
     let encoded_text = encode(&text);
 
     println!("\nEncoding {COUNT} times...");
-    measure(|| {
+    println!("========================");
+    measure(COUNT, || {
         encode(&text);
     });
 
     println!("\nDecoding {COUNT} times...");
-    measure(|| {
+    println!("========================");
+    measure(COUNT, || {
         decode(&encoded_text);
     });
 }
@@ -28,15 +30,32 @@ fn load_text() -> String {
     read_to_string(path).unwrap()
 }
 
-fn measure<F: Fn() -> ()>(func: F) {
-    let now = Instant::now();
+fn measure<F: Fn() -> ()>(count: usize, func: F) {
+    let mut times = vec![];
     {
         for _ in 0..COUNT as u16 {
+            let now = Instant::now();
             func();
+            let elapsed = now.elapsed();
+
+            times.push(elapsed.as_micros());
         }
     }
 
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
-    println!("Average: {:2?}ms", elapsed.as_millis() as f64 / COUNT);
+    let unit = "μs";
+    let elapsed: u128 = times.iter().sum();
+    let max = times.iter().max().unwrap();
+    let min = times.iter().min().unwrap();
+    let avg = elapsed as f64 / count as f64;
+    let sq_difs: Vec<f64> = times
+        .iter()
+        .map(|v| (*v as i128 - avg as i128).pow(2) as f64)
+        .collect();
+    let std_dev = f64::sqrt(sq_difs.iter().sum::<f64>() / count as f64);
+
+    println!("Elapsed: {:.2?}s", elapsed as f64 / 1_000_000.0);
+    println!("Average: {avg:.2}{unit}");
+    println!("Min:     {min:2}{unit}");
+    println!("Max:     {max:2}{unit}");
+    println!("Std dev: {std_dev:.2}{unit}");
 }
